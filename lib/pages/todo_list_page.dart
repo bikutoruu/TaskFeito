727679fe2c7fb638e4943cd66a1ddc78c52f1137
axcse <ozzyvictor@hotmail.com>
2025-05-22 19:34:35 +0900

    Adicionando mais uma Row com 2 filhos, o Texto que vai contar o numero de tarefa que temos e um botão que vai apagar todas as terefas ao mesmo tempo

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -11,21 +11,51 @@ class TodoListPage extends StatelessWidget {
   body: Center(
     child: Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Row(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Adicione uma Tarefa',
-                hintText: 'Ex: Estudar Flutter',
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicione uma Tarefa',
+                    hintText: 'Ex: Estudar Flutter',
+                  ),
+                ),
               ),
-            ),
+              SizedBox(width: 8),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.indigo,
+                  padding: EdgeInsets.all(13),
+                ),
+                child: Icon(
+                  Icons.add,
+                  size: 30,
+                  color: Colors.white,
           ),
-          SizedBox(width: 8),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('+'),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: Text('Você possui 0 tarefas Pendentes'),
+              ),
+              SizedBox(width: 8),
+              ElevatedButton(onPressed: (){},
+              style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.indigo,
+              padding: EdgeInsets.all(13),),
+                child: Text('Limpar Tudo',style: TextStyle(color: Colors.white),),
+    ),
+
+
+            ],
           ),
         ],
       ),
